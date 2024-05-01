35b602740fbdaadd7e95c76746a942da2b040fa2
MSI\Leo <engrleonelfelezario812@gmail.com>
2024-05-01 18:31:15 +0800

    add app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    // Routing configuration
+    provideRouter(routes),
+
+    // Provide HttpClient for making HTTP requests
+    importProvidersFrom(HttpClientModule),
+
+    // Provide HttpClientInMemoryWebApiModule for in-memory data mocking
+    importProvidersFrom([
+      HttpClientInMemoryWebApiModule.forRoot(
+        InMemoryDataService, { dataEncapsulation: false }
+      )
+    ]),
+  ]
 };
